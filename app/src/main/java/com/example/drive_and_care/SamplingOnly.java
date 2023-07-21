9537ee840870f9093ebe642f2785f8723dcd3bb5
D Anuj Kumar <anuj.as828@gmail.com>
2023-07-21 17:58:34 +0530

    sensors restarting

--- app/src/main/java/com/example/drive_and_care/SamplingOnly.java
+++ app/src/main/java/com/example/drive_and_care/SamplingOnly.java
@@ -93,5 +93,96 @@ public class SamplingOnly extends AppCompatActivity {
         updateGPS();
     }
 
+    private void startLocationUpdates() {
+        tv_updates.setText("Location is tracked");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack, null);
+        updateGPS();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case 99:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    updateGPS();
+                }
+                else
+                {
+                    Toast.makeText(this, "This app requires.",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    private void updateGPS(){
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    updateUIValues(location);
+                }
+            });
+        }
+        else
+        {
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},99);
+        }
+    }
+
+    private void updateUIValues(Location location){
+
+        tv_lat.setText(String.valueOf(location.getLatitude()));
+        tv_lon.setText(String.valueOf(location.getLongitude()));
+        tv_accuracy.setText(String.valueOf(location.getAccuracy()));
+        if(location.hasAltitude()){
+            tv_altitude.setText(String.valueOf(location.getAltitude()));
+        }
+        else
+        {
+            tv_altitude.setText("Not available");
+        }
+        if(location.hasSpeed()){
+
+            tv_speed.setText(String.valueOf(location.getSpeed()));
+            if(location.getSpeed()<20.0)
+            {
+                speed_score++;
+            }
+            else if(location.getSpeed() > 20.0)
+            {
+                speed_score += 4;
+            }
+            else if(location.getSpeed() > 40 && location.getSpeed() < 60)
+            {
+                speed_score += 3;
+            }
+            else
+            {
+                speed_score += 2;
+            }
+            textView.setText(Long.toString(speed_score));
+        }
+        else
+        {
+            tv_speed.setText("Not available");
+        }
+        Geocoder geocoder = new Geocoder(this);
+
+
+        try{
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            tv_address.setText(addresses.get(0).getAddressLine(0));
+        }catch (Exception e)
+        {
+            tv_address.setText("Unable to get street address");
+        }
+    }
 
 }

98a009e69ca4dbcfcf5ffc830897114d23b8b223
D Anuj Kumar <anuj.as828@gmail.com>
2023-07-21 12:14:56 +0530

    Sensors initialized

---
+++ app/src/main/java/DatabaseSpeed/params/Params.java
@@ -0,0 +1,7 @@
+package DatabaseSpeed.params;
+
+public class Params {
+    public static final int DB_VERSION = 1;
+    public static final String DB_NAME = "speed_database";
+//    public static final
+}
---
+++ app/src/main/java/com/example/drive_and_care/SamplingOnly.java
@@ -0,0 +1,14 @@
+package com.example.drive_and_care;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SamplingOnly extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sampling_only);
+    }
+}

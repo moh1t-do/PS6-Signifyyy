665a7f91cd072f738fa1749a35d41bb19f428a0d
D Anuj Kumar <anuj.as828@gmail.com>
2023-07-21 10:54:28 +0530

    Speed scoring database initialized

--- app/DrivingScoring.java
+++ app/DrivingScoring.java
@@ -2,8 +2,50 @@ public class DrivingScoring {
     public void speed(){
 
     }
-    public void speedScoring(long totalTime, long highSpeedTime, long ){
+    public void speedScoring(long totalTime, long highSpeedTime, long preScore){
+        double percentage = (highSpeedTime/totalTime) * 100;
+        //Driving scoring model intake from SQLite database system for speed only
+        if(percentage >= 10 && percentage <= 40)
+        {
 
+            //Deduct -1 from speed score system
+            //Aggressive count++
+        }
+        else if(percentage >= 40)
+        {
+            //Deduct -2 from speed score system
+            //Risky count++
+        }
+        else if(percentage <= 10)
+        {
+            //Add +3 from to speed score system
+        }
+        else
+        {
+            //Add +2 from to speed score system
+        }
     }
+    public void accScoring(double acceleration, long highSpeedTime, long preScore){
+        double percentage = (highSpeedTime/totalTime) * 100;
+        //Driving scoring model intake from SQLite database system for speed only
+        if(percentage >= 10 && percentage <= 40)
+        {
 
+            //Deduct -1 from speed score system
+            //Aggressive count++
+        }
+        else if(percentage >= 40)
+        {
+            //Deduct -2 from speed score system
+            //Risky count++
+        }
+        else if(percentage <= 10)
+        {
+            //Add +3 from to speed score system
+        }
+        else
+        {
+            //Add +2 from to speed score system
+        }
+    }
 }

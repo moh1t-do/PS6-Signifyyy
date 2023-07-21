ade999e61ee4ca38c79c4e63a923e963b2de9812
harshdew02 <harshdewangan032@gmail.com>
2023-07-21 16:11:31 +0530

    make working the floating back button

--- app/src/main/java/com/example/drive_and_care/sign_up.java
+++ app/src/main/java/com/example/drive_and_care/sign_up.java
@@ -2,13 +2,33 @@ package com.example.drive_and_care;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class sign_up extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity1();
+            }
+        });
+    }
+
+    public void openActivity1(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
